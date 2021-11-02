b06c3fba06c056dbe40b1aebf9bb614a72c7d4d7
BrenKenna <55492113+BrenKenna@users.noreply.github.com>
2021-11-02 13:52:48 +0000

    added command line actions

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,28 +1,49 @@
 // Import file system module
-const utils = require("./utils.js")
+const utils = require("./notes.js")
 const validator = require("validator")
-console.log(utils.name)
+const notesRoot = "./notes"
+// console.log(utils.name)
 
 
-// Write a file
-utils.createNotes('notes-2.txt', 'Hello again :)')
+// Handle creating note
+const command = process.argv[2];
+if (command.toLowerCase() == "create"){
+    console.log("Attempting to create note");
+    utils.createNotes('notes-2.txt', 'Hello again :)')
 
+} else if (command.toLowerCase() == "read"){
 
-// Read notes
-var note
-console.log("\nLog 1:\n")
-note = utils.readNotes("notes-2.txt")
-console.log(`${note}`)
+    // Handle reading note
+    note = utils.readNotes(`${notesRoot}/notes-2.txt`)
+    console.log(`${note}`)
+
+}else if (command.toLowerCase() == "update"){
+
+    // Handle appending to a note
+    note = utils.appendNotes(`${notesRoot}/notes-2.txt`, "Hello")
+    console.log(`${note}`)
+
+}else if (command.toLowerCase() == "delete"){
+
+    // Handle appending to a note
+    // note = utils.appendNotes(`${notesRoot}/notes-2.txt`, "Hello")
+    // console.log(`${note}`)
+
+} else {
+    console.log("Error only create, get, update and delete are allowed")
+}
 
 
 // Read note 2
-console.log("\n\nLog 2:\n")
-note = utils.readNotes("notes.txt")
-console.log(`${note}`)
+// console.log("\n\nLog 2:\n")
+// note = utils.readNotes(`${notesRoot}/notes.txt`)
+// console.log(`${note}`)
 
 
-// Check an emai
-console.log("\nValidator Checks:")
-console.log(`Hello an email? ${validator.isEmail("Hello")}`)
-console.log(`johnnu@gmail.com an email? ${validator.isEmail("johnny@gmail.com")}`)
-console.log(`https://www.hello.com a URL? ${validator.isURL("https://www.hello.com")}`)
+// Check an email
+// console.log("\nValidator Checks:")
+// console.log(`Hello an email? ${validator.isEmail("Hello")}`)
+// console.log(`johnnu@gmail.com an email? ${validator.isEmail("johnny@gmail.com")}`)
+// console.log(`https://www.hello.com a URL? ${validator.isURL("https://www.hello.com")}`)
+
+
